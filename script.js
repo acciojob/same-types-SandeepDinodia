94cced6ec44e0240ed2bc988e015794eae726089
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2024-05-27 17:44:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 
 function isSameType(value1, value2){
-	if(value1(NaN) && value2(NaN)){
+	if(isNaN(value1)&& isNaN(value2)){
 		return true;
 	}
 	else{
